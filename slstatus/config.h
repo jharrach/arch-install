54fd3e833f6c1b4ed9a2da34ab7557faa0da6673
Jan-Arvid Harrach <jharrach@tutanota.com>
2025-01-06 23:07:44 +0100

    feat(slstatus): optional battery status and notification
    Show if uncommented in config.h:
    battery percentage
    battery state or remaining time
    
    Send notification if uncommented in config.h and either:
    battery energy reached alarm
    battery energy is below alarm an slstatus just started

--- slstatus/config.h
+++ slstatus/config.h
@@ -9,6 +9,8 @@ static const char unknown_str[] = "n/a";
 #define MAXLEN 2048
 
 static const struct arg args[] = {
+	/* { battery_perc, "battery: %s%% ", "<battery>" },
+	{ battery_remaining, "%s | ", "<battery>" }, */
 	{ run_command, "source: %s | ", "pamixer --default-source --get-volume-human" },
 	{ run_command, "sink: %s | ",   "pamixer --get-volume-human" },
 	{ datetime, "%s ",              "%F %R" },
